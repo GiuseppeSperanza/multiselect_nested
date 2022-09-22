18a335d0fe6619a28530328240529f8a5e429681
Giuseppe Speranza <giuseppesperanza1996@gmail.com>
2022-09-22 12:45:56 +0200

    Initial Template Commit

---
+++ lib/multiselect_tree.dart
@@ -0,0 +1,12 @@
+library multiselect_tree;
+
+import 'package:flutter/material.dart';
+
+class MultiSelectTree extends StatelessWidget {
+  const MultiSelectTree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Ciao');
+  }
+}
